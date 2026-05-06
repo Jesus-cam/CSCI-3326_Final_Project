0565d16ff6781ee133fd7b2594898991f143329e
Jesus-cam <jesus.campos03@utrgv.edu>
2026-05-06 01:41:58 +0000

    done

--- warehouse/Battery.java
+++ warehouse/Battery.java
@@ -6,6 +6,21 @@ public class Battery extends Carparts{
         super(name, price, year, model);
     }
 
+
+    @Override
+    public double updated_price() {
+        int current_year = 2026;
+        int yearsPassed = current_year - year;
+        
+        for(int index = 0; index <= yearsPassed; index++) {
+            price = price * .85;
+        }
+
+        return price;
+    }
+
+
+
     public void partInfor() {
         System.out.println("Name: " + name);
         System.out.printf("Price: %.2f\n", updated_price());
